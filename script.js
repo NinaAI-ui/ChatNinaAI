f39f62ad3d74f0ae3a8f4ef1b072d6029d4b3a96
NinaAI-ui <ww.limapatty123@gmail.com>
2025-09-15 11:45:15 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Este é o arquivo JavaScript.
+// Ele será usado para lidar com interações do usuário, como:
+// 1. Enviar a mensagem para o servidor.
+// 2. Mostrar a resposta da IA na tela.
+// 3. Adicionar efeitos visuais, se necessário.
